b35bdf6d434317db90c3cf35eb66ac4165dc8c16
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-08 23:30:25 +0300

    1

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function header() {
+  return (
+   <header>
+    <div>
+      <span className='logo'>
+      Geek
+      </span>
+    </div>
+    <div className='main-img'></div>
+   </header>
+  )
+}
+
+export default header
