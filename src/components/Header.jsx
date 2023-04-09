201c5fa59d7b888466ab442981a72e8b56efdaac
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-09 04:28:17 +0300

    state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-
+import React,{UseState} from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 function Header() {
+  let [cartOpen,SetCartOpen] = UseState(false)
   return (
    <header>
     <div>
       <span className='logo'>
       Geek shop
       </span>
+      
       <ul className='nav'>
         <li>О нас</li>
         <li>Контакты</li>
         <li>Кабинет</li>
       </ul>
+      <ShoppingCartIcon className='ShoppingCartIcon'/>
     </div>
-    <div className='main-img'></div>
+    <div onClick={() => SetCartOpen(cartOpen = !cartOpen)} className='main-img'></div>
    </header>
   )
 }
