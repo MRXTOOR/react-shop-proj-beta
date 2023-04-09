567495571a0af39ac4120bc802bc3d601d498593
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-09 04:10:32 +0300

    ну я и херней же занимаюсь

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import AddIcon from '@mui/icons-material/Add';
 export class Item extends Component {
   render() {
     return (
@@ -8,8 +8,12 @@ export class Item extends Component {
         <h2>{this.props.items.title}</h2>
         
         <p>{this.props.items.desc}</p>
+        <div className='price'>
         <b>{this.props.items.price}$</b>
-        <div className='added'>+</div>
+        </div>
+        <div >
+           <AddIcon className='added'/>
+            </div>
       </div>
     )
   }
